e301f8c48bce639113cda9d55385722f2b2b46ea
Eliud Obure <109294381+Eliud254@users.noreply.github.com>
2024-07-21 22:02:19 +0300

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,18 @@
-
-import redisClient from '../utils/redis';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class AppController {
-  static getStatus(request, response) {
-    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  static getStatus(req, res) {
+    const redisLive = redisClient.isAlive();
+    const dbLive = dbClient.isAlive();
+    res.status(200).json({ redis: redisLive, db: dbLive });
   }
 
-  static async getStats(request, response) {
-    const usersNum = await dbClient.nbUsers();
-    const filesNum = await dbClient.nbFiles();
-    response.status(200).json({ users: usersNum, files: filesNum });
+  static async getStats(req, res) {
+    const usersTotal = await dbClient.nbUsers();
+    const filesTotal = await dbClient.nbFiles();
+    res.status(200).json({ users: usersTotal, files: filesTotal });
   }
 }
 
-module.exports = AppController;
+export default AppController;
